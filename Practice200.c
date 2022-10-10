38788b9a79dbd4153384a7d5398d1f84a083163b
gauravkapruwan <115454124+gauravkapruwan@users.noreply.github.com>
2022-10-10 13:45:29 +0530

    Update Practice200.c
    hacktoberfest2022

--- Practice200.c
+++ Practice200.c
@@ -4,12 +4,12 @@ int main()
 {
     FILE *ptr;
     int  num;
-    int  num2;
+    int  num3;
     ptr = fopen("Sample.txt", "r");    // file read from Sample. txt
     fscanf(ptr, "%d", &num);
-    fscanf(ptr, "%d", &num2);
+    fscanf(ptr, "%d", &num3);
     fclose(ptr);  // closing a file
     printf("The value of num is %d\n", num);
-    printf("The value of num2 is %d\n", num2);
+    printf("The value of num3 is %d\n", num3);
     return 0;
 }
